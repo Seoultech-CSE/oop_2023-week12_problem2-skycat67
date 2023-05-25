e7a6654999a35e18b0917651c7d9231d7468bc43
skycat67 <131930338+skycat67@users.noreply.github.com>
2023-05-25 14:42:45 +0900

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -9,12 +9,12 @@ public class Problem2 {
         int b = input.nextInt();
         int c = input.nextInt();
 
-        /*
-          Rational class를 참고하여 코드 작성
-        */
-      
+        Rational h = new Rational(-b, 2 * a);
+        Rational k = new Rational((4 * a * c - b * b), 4 * a);
+
+        System.out.println("h is " + h + " k is " + k);
     }
-  
+
     static class Rational extends Number implements Comparable<Rational> {
         // Data fields for numerator and denominator
         private long[] r = new long[2];
@@ -27,20 +27,20 @@ public class Problem2 {
         /**Construct a rational with specified numerator and denominator*/
         public Rational(long numerator, long denominator) {
             long gcd = gcd(numerator, denominator);
-            this.r[0] = numerator/gcd;
-            this.r[1] = denominator/gcd;
+            this.r[0] = numerator / gcd;
+            this.r[1] = denominator / gcd;
         }
 
         /**Find GCD of two numbers*/
         private long gcd(long n, long d) {
             long t1 = Math.abs(n);
             long t2 = Math.abs(d);
-            long remainder = t1%t2;
+            long remainder = t1 % t2;
 
             while (remainder != 0) {
                 t1 = t2;
                 t2 = remainder;
-                remainder = t1%t2;
+                remainder = t1 % t2;
             }
 
             return t2;
@@ -58,31 +58,31 @@ public class Problem2 {
 
         /**Add a rational number to this rational*/
         public Rational add(Rational secondNewRational) {
-            long n = r[0]*secondNewRational.getDenominator() +
-                    r[1]*secondNewRational.getNumerator();
-            long d = r[1]*secondNewRational.getDenominator();
+            long n = r[0] * secondNewRational.getDenominator() +
+                    r[1] * secondNewRational.getNumerator();
+            long d = r[1] * secondNewRational.getDenominator();
             return new Rational(n, d);
         }
 
         /**Subtract a rational number from this rational*/
         public Rational subtract(Rational secondNewRational) {
-            long n = r[0]*secondNewRational.getDenominator()
-                    - r[1]*secondNewRational.getNumerator();
-            long d = r[1]*secondNewRational.getDenominator();
+            long n = r[0] * secondNewRational.getDenominator()
+                    - r[1] * secondNewRational.getNumerator();
+            long d = r[1] * secondNewRational.getDenominator();
             return new Rational(n, d);
         }
 
         /**Multiply a rational number to this rational*/
         public Rational multiply(Rational secondNewRational) {
-            long n = r[0]*secondNewRational.getNumerator();
-            long d = r[1]*secondNewRational.getDenominator();
+            long n = r[0] * secondNewRational.getNumerator();
+            long d = r[1] * secondNewRational.getDenominator();
             return new Rational(n, d);
         }
 
         /**Divide a rational number from this rational*/
         public Rational divide(Rational secondNewRational) {
-            long n = r[0]*secondNewRational.getDenominator();
-            long d = r[1]*secondNewRational.r[0];
+            long n = r[0] * secondNewRational.getDenominator();
+            long d = r[1] * secondNewRational.r[0];
             return new Rational(n, d);
         }
 
@@ -96,47 +96,4 @@ public class Problem2 {
 
         /**Override the equals method*/
         public boolean equals(Object parm1) {
-            /**@todo: Override this java.lang.Object method*/
-            if ((this.subtract((Rational)(parm1))).getNumerator() == 0)
-                return true;
-            else
-                return false;
-        }
-
-        /**Override the intValue method*/
-        public int intValue() {
-            /**@todo: implement this java.lang.Number abstract method*/
-            return (int)doubleValue();
-        }
-
-        /**Override the floatValue method*/
-        public float floatValue() {
-            /**@todo: implement this java.lang.Number abstract method*/
-            return (float)doubleValue();
-        }
-
-        /**Override the doubleValue method*/
-        public double doubleValue() {
-            /**@todo: implement this java.lang.Number abstract method*/
-            return r[0]*1.0/r[1];
-        }
-
-        /**Override the longValue method*/
-        public long longValue() {
-            /**@todo: implement this java.lang.Number abstract method*/
-            return (long)doubleValue();
-        }
-
-        @Override
-        public int compareTo(Rational o) {
-            /**@todo: Implement this java.lang.Comparable method*/
-            if ((this.subtract((Rational)o)).getNumerator() > 0)
-                return 1;
-            else if ((this.subtract((Rational)o)).getNumerator() < 0)
-                return -1;
-            else
-                return 0;
-        }
-    }
-
-}
+            if ((this.subtract((Rational) (parm1))).getNumerator() == 0)
